5520426e8461d58d398cd7b6fc2f55ef4d546ea9
nitin23c <nitin.s.chauhan@gmail.com>
2024-02-24 20:55:54 +0530

    Image scan script added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
-def call(String project, String imageTag, String hubUser){
+def call(String imageName, String imageTag, String hubUser){
     sh """
-     docker image build -t ${hubUser}/${project} .
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+     docker image build -t ${hubUser}/${imageName} .
+     docker image tag ${hubUser}/${imageName} ${hubUser}/${imageName}:${imageTag}
+     docker image tag ${hubUser}/${imageName} ${hubUser}/${imageName}:latest
     """
 }
--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,6 @@
 def call(String imageName, String imageTag, String hubUser){
     sh """
-     trivy image ${hubUser}/${imageName} ${hubUser}/${imageName}:latest > scan.txt
+     trivy image ${hubUser}/${imageName}:latest > scan.txt
      cat scan.txt
     """
 }
