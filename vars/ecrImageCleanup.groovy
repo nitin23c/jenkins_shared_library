73a0eb4b412c79177785db73384e2d8f828ad62f
nitin23c <nitin.s.chauhan@gmail.com>
2024-02-25 00:36:45 +0530

    Clean local images

--- vars/ecrImageCleanup.groovy
+++ vars/ecrImageCleanup.groovy
@@ -1,5 +1,6 @@
 def call(String aws_account_id, String region, String ecr_repo_name){
     sh """
      docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest
+     docker rmi ${ecr_repo_name}:latest
     """
 }
